bd8db676e008f446f0e84c6743f246d5f611f297
Hatice Günay <hatice.turegun@gmail.com>
2024-01-09 11:15:30 +0300

    Turkish characters corrected

--- src/main/java/org/example/Gezegen.java
+++ src/main/java/org/example/Gezegen.java
@@ -1,14 +1,14 @@
 package org.example;
 
 public enum Gezegen {
-    MERKÜR(1, 10.1, 1000.7, 5.0),
-    VENüS(2, 90.2, 5000.8, 100.0),
+    MERCURY(1, 10.1, 1000.7, 5.0),
+    VENUS(2, 90.2, 5000.8, 100.0),
     EARTH(3, 110.2, 5000.0, 5.0),
     MARS(4, 150.9, 3000.5, 5.0),
-    JÜPİTER(5, 300.5, 10000.0, 0.7),
-    SATÜRN(6, 1200.4, 55555.0, 0.4),
-    URANÜS(7, 122.5, 25000.0, 3.7),
-    NEPTÜNE(8, 2000.1, 20000.0, 0.3);
+    JUPITER(5, 300.5, 10000.0, 0.7),
+    SATURN(6, 1200.4, 55555.0, 0.4),
+    URANUS(7, 122.5, 25000.0, 3.7),
+    NEPTUNE(8, 2000.1, 20000.0, 0.3);
 
     private int siralamaNumarasi;
     private double uzaklik;
